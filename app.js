53a75fd34cfe9420bca93003f69924df64ce6798
mohammad ahmadi <mohammad32.ahmadi@gmail.com>
2022-10-15 07:58:57 +0200

    configure github action

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 App.use(express.static(path.join(__dirname , 'frontend/build')))
 
-const PORT = process.env.PORT | 8000;
+const PORT = process.env.PORT | 5000;
 
 App.get('/names', async(req , res) =>{
     res.send("Mohammad ahmadi you are the one");
